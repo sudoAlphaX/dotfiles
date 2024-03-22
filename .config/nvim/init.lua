01b4054d40349b754385e2305fbebc7bec360e8c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-03-22 18:26:04 +0530

    initial dotfiles setup

---
+++ .config/nvim/init.lua
@@ -0,0 +1,1 @@
+../templates/nvim/init.lua
---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,13 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "onedark",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
