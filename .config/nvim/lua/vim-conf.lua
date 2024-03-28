1a2c54aced7f79b6c7729a18520e2f836a66546e
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-03-29 01:02:13 +0530

    enable line numbers and mouse

--- .config/nvim/lua/vim-conf.lua
+++ .config/nvim/lua/vim-conf.lua
@@ -1,7 +1,12 @@
+-- Vim global configs
+
+-- Set tabs to use 4 spaces and replace tab characters to spaces
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
 vim.g.mapleader = " "
 
-
+vim.cmd("set number")
+vim.cmd("set mouse=a")
