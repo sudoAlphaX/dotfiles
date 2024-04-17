e95d8f96042d53aa7c28470412f53bf003c37c94
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-04-17 16:47:37 +0530

    add search and code folding customization

--- .config/nvim/lua/vim-conf.lua
+++ .config/nvim/lua/vim-conf.lua
@@ -5,8 +5,24 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set autoindent")
+vim.cmd("set autowrite")
 
 vim.g.mapleader = " "
 
 vim.cmd("set number")
 vim.cmd("set mouse=a")
+
+-- Search options
+vim.cmd("set hlsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set incsearch")
+vim.cmd("set smartcase")
+
+-- Text rendering options
+vim.cmd("set cursorline")
+
+-- Code folding
+vim.cmd("set foldmethod=indent")
+vim.cmd("set nofoldenable")
+vim.cmd("set foldnestmax=3")
