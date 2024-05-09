900277d870f097b1d5dab1f9524722658eb49bbc
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-09 22:17:49 +0530

    refactor which-key with 4 spaced indent
    (cherry picked from commit 1babd75029950357958fa4eeec6f97b16d54681a)

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,34 +1,34 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
 
-    local wk = require("which-key")
-        wk.register({
-            ["<leader>"] = {
-                o = { "<cmd>Telescope find_files<cr>", "Open Files" },
-                f = {
-                    name = "+file",
-                    g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-                    r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
-            },
-                g = {
-                    name = "+git",
-                    s = { "<cmd>Telescope git_status<cr>", "Git Status" },
-                    b = { "<cmd>Telescope git_branches<cr>", "Git Branches" },
-                    c = { "<cmd>Telescope git_commits<cr>", "Git Commits" },
-                    f = { "<cmd>Telescope git_files<cr>", "Find Git Files" }
-            },
-                b = { "<cmd>Telescope buffers<cr>", "List Buffers" },
-        }
-    })
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+        local wk = require("which-key")
+            wk.register({
+                ["<leader>"] = {
+                    o = { "<cmd>Telescope find_files<cr>", "Open Files" },
+                    f = {
+                        name = "+file",
+                        g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+                        r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
+                    },
+                    g = {
+                        name = "+git",
+                        s = { "<cmd>Telescope git_status<cr>", "Git Status" },
+                        b = { "<cmd>Telescope git_branches<cr>", "Git Branches" },
+                        c = { "<cmd>Telescope git_commits<cr>", "Git Commits" },
+                        f = { "<cmd>Telescope git_files<cr>", "Find Git Files" }
+                    },
+                    b = { "<cmd>Telescope buffers<cr>", "List Buffers" },
                     v = { "<cmd>Telescope neoclip<cr>", "Register History" }
+              }
+        })
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
 }
