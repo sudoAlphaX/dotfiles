8b44fdc57c70d172f95ac28797016af7b6d8343e
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-09 16:09:39 +0530

    add which-key and configure telescope keybinds in which-key

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,13 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
+--[[
     config = function()
         local configs = require("telescope.builtin")
         vim.keymap.set("n", "<leader>o", configs.find_files, {})
         vim.keymap.set("n", "<leader>g", configs.live_grep, {})
         vim.keymap.set("n", "<leader>b", configs.buffers, {})
     end
+]]
 }
+
