68f2ce5e64278b3a7ef7a42d360249671e3feb34
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-23 23:24:00 +0530

    add cmp plugin and sources

---
+++ .config/nvim/lua/plugins/cmp-setup.lua
@@ -0,0 +1,47 @@
+return {
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+		dependencies = {
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
+		},
+		config = function()
+			local configs = require("cmp")
+			configs.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				window = {
+					completion = configs.config.window.bordered(),
+					documentation = configs.config.window.bordered(),
+				},
+				mapping = configs.mapping.preset.insert({
+					["<C-b>"] = configs.mapping.scroll_docs(-4),
+					["<C-f>"] = configs.mapping.scroll_docs(4),
+					["<C-Space>"] = configs.mapping.complete(),
+					["<C-e>"] = configs.mapping.abort(),
+					["<CR>"] = configs.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = configs.config.sources({
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+}
