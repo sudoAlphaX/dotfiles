59d44dbe03873359cb6898d8f851afe27ce20fb0
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-23 21:57:56 +0530

    change close buffer to prefix b (<leader>bx)

--- .config/nvim/lua/plugins/bufferline_nvim.lua
+++ .config/nvim/lua/plugins/bufferline_nvim.lua
@@ -17,6 +17,6 @@ return {
         configs.setup()
         vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
         vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
-        vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>", { desc = "Close Buffer"})
+        vim.keymap.set("n", "<leader>bx", "<cmd>bd<CR>", { desc = "Close Buffer"})
     end,
 }
