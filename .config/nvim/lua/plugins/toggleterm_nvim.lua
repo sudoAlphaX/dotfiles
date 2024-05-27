0ebb961d64256434f79744ecc7c09cc479be139b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-27 16:33:33 +0530

    setup toggleterm

--- .config/nvim/lua/plugins/toggleterm_nvim.lua
+++ .config/nvim/lua/plugins/toggleterm_nvim.lua
@@ -1,5 +1,12 @@
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-	config = true,
+	config = function()
+		local configs = require("toggleterm")
+		configs.setup()
+		vim.keymap.set("n", "<leader>tt", "<CMD>ToggleTerm direction=horizontal<CR>", { desc = "Terminal split" })
+		vim.keymap.set("n", "<leader>tf", "<CMD>ToggleTerm direction=float<CR>", { desc = "Floating Terminal" })
+		vim.keymap.set("n", "<leader>tb", "<CMD>ToggleTerm direction=tab<CR>", { desc = "Terminal in a Tab" })
+		vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]])
+	end,
 }
