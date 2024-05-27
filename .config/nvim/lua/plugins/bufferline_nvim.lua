2f739e6b3fd4243e8c04126d5aa62a6b066e2d46
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-27 16:10:40 +0530

    relocate buffer mapping and create new buffer map
    (cherry picked from commit 9936b9d542c0db322df2211b1d41ce91811f8e96)

--- .config/nvim/lua/plugins/bufferline_nvim.lua
+++ .config/nvim/lua/plugins/bufferline_nvim.lua
@@ -7,6 +7,5 @@ return {
 		configs.setup()
 		vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
 		vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
-		vim.keymap.set("n", "<leader>bx", "<cmd>bd<CR>", { desc = "Close Buffer" })
 	end,
 }
