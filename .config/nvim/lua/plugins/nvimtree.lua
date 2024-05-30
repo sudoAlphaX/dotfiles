5570cd4e3d927f5c277438ce2dc18d35df59d859
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-05-30 22:12:29 +0530

    open nvimtree on the right

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -6,11 +6,13 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 		vim.opt.termguicolors = true
 
+		require("nvim-tree").setup({
+			view = { side = "right" },
+		})
 
 		local configs = require("nvim-tree.api")
 		vim.keymap.set("n", "<C-n>", configs.tree.toggle)
