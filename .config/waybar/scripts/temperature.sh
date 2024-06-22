e2431a76e5fc042e326b0d4bdd70d3a96d84ffaa
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-06-22 22:11:28 +0530

    waybar: add states to temperature module

--- .config/waybar/scripts/temperature.sh
+++ .config/waybar/scripts/temperature.sh
@@ -6,6 +6,20 @@ if [[ -z "$path" ]]; then
 	echo "Path not specified. Exiting..."
 	exit 1
 else
-	temp=$(cat "$path"/hwmon[[:print:]]*/temp1_input)
-	echo "${temp::-3}" # remove last 3 characters
+	rawTemp=$(cat "$path"/hwmon[[:print:]]*/temp1_input)
+	temp=${rawTemp::-3}
+
+	criticalTemp=68
+	highTemp=60
+
+	if (( $temp >= $criticalTemp )); then
+		tempState="critical"
+	elif (( $temp >= $highTemp )); then
+		tempState="high"
+	else
+		tempState="normal"
+	fi
+
+	printf '{"text": "%s", "class": "%s"}\n' "$temp" "$tempState"
+
 fi
