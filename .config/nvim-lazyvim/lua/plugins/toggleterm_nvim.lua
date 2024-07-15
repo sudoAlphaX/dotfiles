cbdb67f4c90af9241b03edc5da078f250f178570
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-15 21:53:52 +0530

    lazyvim: configure toggleterm plugin
    (cherry picked from commit 8a37096ada1b00ebcfd5b4d9d993e55b49dd55c7)

---
+++ .config/nvim-lazyvim/lua/plugins/toggleterm_nvim.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    { "<leader>tt", "<CMD>ToggleTerm direction=horizontal<CR>", desc = "Terminal split" },
+    { "<leader>tf", "<CMD>ToggleTerm direction=float<CR>", desc = "Floating Terminal" },
+    { "<leader>tb", "<CMD>ToggleTerm direction=tab<CR>", desc = "Terminal in a Tab" },
+  },
+  config = true,
+}
