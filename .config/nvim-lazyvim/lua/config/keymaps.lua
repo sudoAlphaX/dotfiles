6fe48b478f192247e2d2f21bc3f05d6ce6fff561
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-15 23:07:09 +0530

    lazyvim: buffer manipulation keymaps

--- .config/nvim-lazyvim/lua/config/keymaps.lua
+++ .config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+-- Bufferline Tabs
+map("n", "<Tab>", "<CMD>BufferLineCycleNext<CR>")
+map("n", "<S-Tab>", "<CMD>BufferLineCyclePrev<CR>")
+unmap("n", "<leader>bd")
+
+-- Buffers
+map("n", "<leader>bn", "<CMD>enew<CR>", { desc = "New Buffer" })
+map("n", "<leader>bx", "<CMD>bd<CR>", { desc = "Close Buffer" })
+map("n", "<leader>bX", "<CMD>bd!<CR>", { desc = "Force Close Buffer" })
