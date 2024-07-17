2c3c2a264c71dd5a49f795dad2eace4e3f29a851
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-17 18:34:03 +0530

    lazyvim: add neoclip plugin

---
+++ .config/nvim-lazyvim/lua/plugins/neoclip.lua
@@ -0,0 +1,10 @@
+return {
+  "AckslD/nvim-neoclip.lua",
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" },
+  },
+  keys = {
+    { "<leader>p", "<CMD>Telescope neoclip<CR>" },
+  },
+  config = true,
+}
