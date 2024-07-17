4c4210c030c23f8da6d008fa9a9c6f9d471cdce6
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-17 17:54:02 +0530

    lazyvim: put visual mode keymaps inside keys{}

--- .config/nvim-lazyvim/lua/plugins/comment_nvim.lua
+++ .config/nvim-lazyvim/lua/plugins/comment_nvim.lua
@@ -1,8 +1,8 @@
 return {
   "numToStr/Comment.nvim",
   keys = {
-    { "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise.current()<CR>" },
+    { "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise.current()<CR>", mode = { "n" } },
+    { "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", mode = { "v" } },
   },
   config = true,
-  vim.keymap.set("x", "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>"),
 }
