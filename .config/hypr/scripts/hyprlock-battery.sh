e0adf0e508b1e6323ae18e0ae654c0a6c923d5ea
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-04 13:55:51 +0530

    hypr: fix indentation and code warning

--- .config/hypr/scripts/hyprlock-battery.sh
+++ .config/hypr/scripts/hyprlock-battery.sh
@@ -1,27 +1,28 @@
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
-if (( capacity > 80 )); then
-	batticon=" "
-elif (( capacity > 60 )); then
-	batticon=" "
-elif (( capacity > 40 )); then
-	batticon=" "
-elif (( capacity > 20 )); then
-	batticon=" "
+if ((capacity > 80)); then
+  batticon=" "
+elif ((capacity > 60)); then
+  batticon=" "
+elif ((capacity > 40)); then
+  batticon=" "
+elif ((capacity > 20)); then
+  batticon=" "
 else
-	batticon=" "
+  batticon=" "
 fi
 
 if [[ "$status" == "Charging" ]]; then
-	icon=" $batticon"
+  icon=" $batticon"
 elif [[ "$status" == "Discharging" ]]; then
-	icon="$batticon"
+  icon="$batticon"
 elif [[ "$status" == "Not Charging" ]]; then
-	icon="󰚦 $batticon"
-elif [[ "status" == "Full" ]]; then
-	icon=" $batticon"
+  icon="󰚦 $batticon"
+elif [[ "$status" == "Full" ]]; then
+  icon=" $batticon"
 else
-	icon="$batticon"
+  icon="$batticon"
+fi
 
-echo $icon
+echo "$icon"
--- .config/hypr/scripts/screen-record.sh
+++ .config/hypr/scripts/screen-record.sh
@@ -3,48 +3,45 @@
 pid=$(pidof wf-recorder)
 
 if [ "$pid" ]; then
-	# Stop screen recording
-	for id in $pid; do 
-		while kill -INT $id; do
-			sleep 1
-		done
-	done
-	
-	pactl unload-module module-loopback
-	pactl unload-module module-null-sink
+  # Stop screen recording
+  for id in $pid; do
+    while kill -INT "$id"; do
+      sleep 1
+    done
+  done
 
-	exit 0
+  pactl unload-module module-loopback
+  pactl unload-module module-null-sink
+
+  exit 0
 fi
-
 
 declare -a monitors
 
 for source in $(pactl list sources | grep Name | grep monitor | sed 's/Name: //g'); do
-	monitors+=("$source")
+  monitors+=("$source")
 done
-
 
 pactl load-module module-null-sink sink_name=combined
 
-for source in "${monitors[@]}"; do 
-	pactl load-module module-loopback sink=combined source=$source
+for source in "${monitors[@]}"; do
+  pactl load-module module-loopback sink=combined source="$source"
 done
 
 mkdir -p "$HOME/Videos/wf-recorder/tmp"
 
 filename="$(date +%Y%m%d_%H%M%S)_$(hyprctl activewindow -j | jq -r .class).mp4"
-filepath="$HOME/Videos/wf-recorder$(if [ $1 != "false" ]; then echo "/tmp"; fi)/$filename"
+filepath="$HOME/Videos/wf-recorder$(if [ "$1" != "false" ]; then echo "/tmp"; fi)/$filename"
 
-
-if [ $2 = "true" ]; then
-	wf-recorder -g "$(slurp)" --audio=combined.monitor -f $filepath -r 30 -c h264_vaapi -d /dev/dri/renderD128
+if [ "$2" = "true" ]; then
+  wf-recorder -g "$(slurp)" --audio=combined.monitor -f "$filepath" -r 30 -c h264_vaapi -d /dev/dri/renderD128
 else
-	wf-recorder --audio=combined.monitor -f $filepath -r 30 -c h264_vaapi -d /dev/dri/renderD128
+  wf-recorder --audio=combined.monitor -f "$filepath" -r 30 -c h264_vaapi -d /dev/dri/renderD128
 fi
 
-if [ $1 != "false" ]; then
-	wl-copy "file://$filepath" --type "text/uri-list"
-	notify-send -u low -a wf-recorder "Copied screen recording to clipboard"
+if [ "$1" != "false" ]; then
+  wl-copy "file://$filepath" --type "text/uri-list"
+  notify-send -u low -a wf-recorder "Copied screen recording to clipboard"
 else
-	notify-send -u low -a wf-recorder "Saved screen recording to $filename"
+  notify-send -u low -a wf-recorder "Saved screen recording to $filename"
 fi
