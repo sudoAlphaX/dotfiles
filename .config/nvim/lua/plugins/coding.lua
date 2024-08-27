746c32c71c1d9912ad130b1b59f980ddfd80c889
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-27 22:00:11 +0530

    nvim: add telescope and keybinds

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -20,4 +20,30 @@ return {
 			})
 		end,
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		lazy = true,
+		keys = {
+			-- Find Files
+			{ "<leader> ", "<CMD>lua require('telescope.builtin').find_files()<CR>", { desc = "Open File" } },
+			{ "<leader>o", "<CMD>lua require('telescope.builtin').git_files()<CR>", { desc = "Git Files" } },
+			{ "<leader>r", "<CMD>lua require('telescope.builtin').oldfiles()<CR>", { desc = "Recent Files" } },
+			{ "<leader>/", "<CMD>lua require('telescope.builtin').live_grep()<CR>", { desc = "Live Grep" } },
+
+			-- Buffers
+			{ "<leader>bb", "<CMD>lua require('telescope.builtin').buffers()<CR>", { desc = "List Buffers" } },
+
+			{ "<leader>p", "<CMD>lua require('telescope.builtin').registers()<CR>", { desc = "List Registers" } },
+
+			-- Git
+			{ "<leader>gc", "<CMD>lua require('telescope.builtin').git_commits()<CR>", { desc = "Git Commits" } },
+			{ "<leader>gb", "<CMD>lua require('telescope.builtin').git_branches()<CR>", { desc = "Git Branches" } },
+			{ "<leader>gs", "<CMD>lua require('telescope.builtin').git_status()<CR>", { desc = "Git Status" } },
+			{ "<leader>gh", "<CMD>lua require('telescope.builtin').git_stash()<CR>", { desc = "Git Stashes" } },
+		},
+	},
 }
