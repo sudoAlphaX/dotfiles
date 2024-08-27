34ddd77d315fd0d97cd7d92ae685de1e851208dd
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-27 21:13:26 +0530

    nvim: add indentation shortcuts

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,6 @@ map("n", "<leader>bn", "<CMD>enew<CR>", { desc = "New Buffer" })
 map("n", "<leader>bx", "<CMD>bd<CR>", { desc = "Close Buffer" })
 map("n", "<leader>bX", "<CMD>bd!<CR>", { desc = "Force Close Buffer" })
 
+-- Indentation shortcuts
+map("x", "<Tab>", ">gv")
+map("x", "<S-Tab>", "<gv")
