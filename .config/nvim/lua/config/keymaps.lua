0ef83e17491114326d8a94d32a4db5ce89daa8f9
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-27 21:13:19 +0530

    nvim: add buffer keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
+-- Buffers
+map("n", "<Tab>", "<CMD>bnext<CR>")
+map("n", "<S-Tab>", "<CMD>bprevious<CR>")
+map("n", "b0", "<CMD>bfirst<CR>", { desc = "Go to First Buffer" })
+map("n", "b$", "<CMD>blast<CR>", { desc = "Go to Last Buffer" })
+
+map("n", "<leader>bn", "<CMD>enew<CR>", { desc = "New Buffer" })
+map("n", "<leader>bx", "<CMD>bd<CR>", { desc = "Close Buffer" })
+map("n", "<leader>bX", "<CMD>bd!<CR>", { desc = "Force Close Buffer" })
+
