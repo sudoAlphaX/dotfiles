e454840d68e1928ee90230b46dfd6f5730fcf7c5
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-27 16:45:21 +0530

    nvim: add lualine with basic configs

---
+++ .config/nvim/lua/plugins/bars.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		config = function()
+			require("lualine").setup({
+				options = { theme = "catppuccin" },
+			})
+		end,
+	},
+}
