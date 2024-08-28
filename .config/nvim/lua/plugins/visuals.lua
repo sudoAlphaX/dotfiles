a31abad87684ea562438f6420d843d02cf973704
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-28 17:41:48 +0530

    nvim: add groups and descriptions for which-key

--- .config/nvim/lua/plugins/visuals.lua
+++ .config/nvim/lua/plugins/visuals.lua
@@ -11,7 +11,43 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			spec = {
+				{
+					mode = { "n", "v" },
+					-- { "<leader>c", group = "code" },
+					-- { "<leader>f", group = "file/find" },
+					{ "<leader>g", group = "git" },
+					{ "<leader>s", group = "search" },
+					{ "<leader>t", group = "terminal", icon = { icon = " ", color = "purple" } },
+					{ "<leader>z", icon = { icon = "", color = "blue" } },
+					-- { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+					{ "[", group = "prev" },
+					{ "]", group = "next" },
+					{ "g", group = "goto" },
+					{ "gs", group = "surround" },
+					{ "z", group = "fold" },
+					{
+						"<leader>b",
+						group = "buffer",
+						expand = function()
+							return require("which-key.extras").expand.buf()
+						end,
+					},
+					{
+						"<leader>w",
+						group = "windows",
+						proxy = "<c-w>",
+						expand = function()
+							return require("which-key.extras").expand.win()
+						end,
+					},
+					-- better descriptions
+					{ "gx", desc = "Open with system app" },
+				},
+			},
+		},
+
 		keys = {
 			{
 				"<leader>?",
