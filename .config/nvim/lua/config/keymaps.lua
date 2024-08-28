ff303aacb3df5c21845b3c0f17042d8d499ceac4
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-28 17:04:48 +0530

    nvim: resize windows using <C-arrow_keys>
    https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/config/keymaps.lua#L19C1-L24C1

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,3 +22,9 @@ map("x", "<S-Tab>", "<gv")
 -- Window Splits
 map("n", "<leader>-", "<CMD>split<CR>", { desc = "Split Window Below" })
 map("n", "<leader>|", "<CMD>vsplit<CR>", { desc = "Split Window Right" })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
