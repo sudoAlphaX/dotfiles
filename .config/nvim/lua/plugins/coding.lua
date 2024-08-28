d57ef8db3b67ff813505bff8815dfe01f98b53cc
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-28 16:50:06 +0530

    nvim: add grug-far plugin for find and replace
    yanked from lazyvim: https://www.lazyvim.org/plugins/editor#grug-farnvim

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -90,4 +90,27 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
 	},
+	{
+		"MagicDuck/grug-far.nvim",
+		opts = { windowCreationCommand = "botright vsplit" },
+		cmd = "GrugFar",
+		keys = {
+			{
+				"<leader>sr",
+				function()
+					local grug = require("grug-far")
+					local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+					grug.grug_far({
+						transient = true,
+						prefills = {
+							filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+						},
+					})
+				end,
+				mode = { "n", "v" },
+				desc = "Search and Replace",
+			},
+		},
+		lazy = true,
+	},
 }
