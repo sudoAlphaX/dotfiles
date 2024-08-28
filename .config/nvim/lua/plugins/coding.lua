27d8813e6b342dfcb487580585a2bec45689e0f5
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-28 16:48:51 +0530

    nvim: move toggleterm inside coding

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -61,4 +61,18 @@ return {
 			})
 		end,
 	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		keys = {
+			{ "<leader>tt", "<CMD>ToggleTerm direction=horizontal<CR>", desc = "Terminal split" },
+			{ "<leader>tf", "<CMD>ToggleTerm direction=float<CR>", desc = "Floating Terminal" },
+			{ "<leader>tb", "<CMD>ToggleTerm direction=tab<CR>", desc = "Terminal in a Tab" },
+
+			-- Lazygit
+			-- { "<leader>gg", "<CMD>TermExec name=Lazygit cmd=lazygit direction=float<CR>", desc = "Lazygit" },
+		},
+		lazy = true,
+		config = true,
+	},
 }
