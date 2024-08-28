6d5ae1f182da3141df941960664d371e37128676
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-28 17:27:16 +0530

    nvim: add which-key plugin

--- .config/nvim/lua/plugins/visuals.lua
+++ .config/nvim/lua/plugins/visuals.lua
@@ -8,4 +8,18 @@ return {
 	{
 		"RRethy/vim-illuminate",
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
