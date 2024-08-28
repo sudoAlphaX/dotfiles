e49b0041767d969dc2ddab39ce2898a45f5431c3
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-28 18:11:47 +0530

    nvim: add luasnip plugin and friendly snippets
    https://www.lazyvim.org/extras/coding/luasnip#luasnip-1

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -62,4 +62,34 @@ return {
 			}
 		end,
 	},
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			{
+				"nvim-cmp",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+				},
+				opts = function(_, opts)
+					opts.snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					}
+				end,
+			},
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+	},
 }
