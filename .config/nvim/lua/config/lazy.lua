491b4800ab1eab7185729181847ab917516c2158
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-29 21:00:18 +0530

    nvim: add custom plugins directory and custom settings for creating new configs

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "plugins" },
+    { import = "custom.plugins"}
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
