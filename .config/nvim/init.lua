d57137b1533add95121c5937f17b026a551fefef
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-29 21:00:18 +0530

    nvim: add custom plugins directory and custom settings for creating new configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,8 @@ require("config.lazy")
 require("config.keymaps")
 require("config.options")
 require("config.autocmds")
+
+-- Custom configs
+require("custom.config.keymaps")
+require("custom.config.options")
+require("custom.config.autocmds")
