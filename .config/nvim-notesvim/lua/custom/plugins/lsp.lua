014d0079986df99a7cc74ac9eb782f4041bd5b2d
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-31 14:28:00 +0530

    notesvim: add markdown lsp

---
+++ .config/nvim-notesvim/lua/custom/plugins/lsp.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		opts = {
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- Lua
+					"lua_ls",
+					"stylua",
+					"marksman",
+				},
+				auto_update = true,
+			}),
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+
+			local lsp = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			lsp.lua_ls.setup({ capabilities = capabilities })
+			lsp.marksman.setup({ capabilities = capabilities })
+		end,
+	},
+}
