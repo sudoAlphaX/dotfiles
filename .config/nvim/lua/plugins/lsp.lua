f6a276a113e9914d10bc93128bf3bc7b45612b4b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-31 13:57:25 +0530

    nvim: setup mason and lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,60 @@
 return {
-  { 'MeanderingProgrammer/render-markdown.nvim',
-  opts = {preset = 'lazy'},
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-  lazy = true,
-  ft = { "markdown" }
-  }
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = { preset = "lazy" },
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		lazy = true,
+		ft = { "markdown" },
+	},
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		build = ":MasonUpdate",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts_extend = { "ensure_installed" },
+		opts = {
+			ensure_installed = {
+				"stylua",
+				-- "shfmt",
+			},
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			mr:on("package:install:success", function()
+				vim.defer_fn(function()
+					-- trigger FileType event to possibly load this newly installed LSP server
+					require("lazy.core.handler.event").trigger({
+						event = "FileType",
+						buf = vim.api.nvim_get_current_buf(),
+					})
+				end, 100)
+			end)
+
+			mr.refresh(function()
+				for _, tool in ipairs(opts.ensure_installed) do
+					local p = mr.get_package(tool)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end)
+		end,
+	},
+	{ "williamboman/mason-lspconfig.nvim", config = function() end },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+
+			local lsp = require("lspconfig")
+
+			lsp.lua_ls.setup({})
+		end,
+	},
 }
