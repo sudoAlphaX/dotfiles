73ae021f651599c21a0d85c32d7e6c44b4e113b1
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-31 20:41:48 +0530

    nvim: add nvim-lint plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -127,4 +127,20 @@ return {
 		},
 		lazy = true,
 	},
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					-- try_lint without arguments runs the linters defined in `linters_by_ft`
+					-- for the current filetype
+					require("lint").try_lint()
+
+					-- You can call `try_lint` with a linter name or a list of names to always
+					-- run specific linters, independent of the `linters_by_ft` configuration
+					require("lint").try_lint("cspell")
+				end,
+			})
+		end,
+	},
 }
