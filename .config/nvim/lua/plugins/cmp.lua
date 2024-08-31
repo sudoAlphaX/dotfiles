0ed57c6f5696ffba10b6c94bf13dae2837e96f4b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-31 13:19:46 +0530

    nvim: add luasnip custom snippets and jump using tab

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -7,7 +7,20 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "hrsh7th/nvim-cmp",
       "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true, silent = true, mode = "i",
+      },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
@@ -53,6 +66,10 @@ return {
             return item
           end,
         },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
         experimental = {
           ghost_text = {
             hl_group = "CmpGhostText",
@@ -79,6 +96,12 @@ return {
           "saadparwaiz1/cmp_luasnip",
         },
         opts = function(_, opts)
+          require("luasnip.loaders.from_vscode").lazy_load({
+            paths = "./lua/config/snippets",
+          })
+          require("luasnip.loaders.from_vscode").lazy_load({
+            paths = "./lua/custom/config/snippets",
+          })
           opts.snippet = {
             expand = function(args)
               require("luasnip").lsp_expand(args.body)
