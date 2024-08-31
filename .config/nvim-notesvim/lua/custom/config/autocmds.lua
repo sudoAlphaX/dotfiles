f786a12dca71f3b5e95ff9b6b6bf648ff9fd157d
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-31 21:50:35 +0530

    notesvim: autosave autocmd

--- .config/nvim-notesvim/lua/custom/config/autocmds.lua
+++ .config/nvim-notesvim/lua/custom/config/autocmds.lua
@@ -1,2 +1,24 @@
 -- add tex snippets to markdown files for friendly-snippets
 require("luasnip").filetype_extend("markdown", { "tex" })
+
+-- Autosave
+
+local function save()
+	local buf = vim.api.nvim_get_current_buf()
+
+	vim.api.nvim_buf_call(buf, function()
+		vim.cmd("silent! write")
+	end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+	callback = function()
+		save()
+	end,
+	pattern = "*",
+	group = "AutoSave",
+})
