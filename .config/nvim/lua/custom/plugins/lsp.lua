679c2df9842076133d60d80a1929de9b6db24b20
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-09-04 15:25:06 +0530

    nvim: mason auto attach lsp

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -56,7 +56,12 @@ return {
 
 			lsp.clangd.setup({ capabilities = capabilities })
 
-			lsp.arduino_language_server.setup({ capabilities = capabilities })
+			require("mason-lspconfig").setup_handlers({
+
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({})
+				end,
+			})
 		end,
 	},
 }
