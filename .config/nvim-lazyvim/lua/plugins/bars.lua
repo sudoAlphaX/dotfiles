04f4f74e5dd49c35be8b4babc41f59ae90aa02d8
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-10-27 22:01:23 +0530

    lazyvim: remove neo-tree in favor of nvimtree

--- .config/nvim-lazyvim/lua/plugins/bars.lua
+++ .config/nvim-lazyvim/lua/plugins/bars.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      extensions = { "nvim-tree" },
+    },
+  },
+  {
     "akinsho/bufferline.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
     opts = {
