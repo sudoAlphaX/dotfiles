2cdf98453d2c51dc454af19d33b33c1def7a1210
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-10-27 22:03:00 +0530

    lazyvim: remove comment_nvim in favor of native functionality

--- .config/nvim-lazyvim/lua/plugins/comment_nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "numToStr/Comment.nvim",
-  keys = {
-    { "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise.current()<CR>", mode = { "n" } },
-    { "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", mode = { "v" } },
-  },
-  lazy = true,
-}
