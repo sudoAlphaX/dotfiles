1589c6a107d42da9996dd372291bcc2d9d0b4cc4
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-10-27 22:00:50 +0530

    lazyvim: always show bufferline

---
+++ .config/nvim-lazyvim/lua/plugins/bars.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
+}
