ca5d2c0b1d3f301793325c4735c0b8b106a5c85c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-10-28 13:25:40 +0530

    dunst: add icons for touchscreen toggle

--- .config/hypr/scripts/touchscreen-toggle.sh
+++ .config/hypr/scripts/touchscreen-toggle.sh
@@ -5,13 +5,13 @@ STATUS_FILE=/tmp/touchscreen.log
 enable_touchscreen() {
   hyprctl keyword -r '$TOUCHSCREEN_ENABLED' "true"
   echo "true" >"$STATUS_FILE"
-  notify-send --category=device --urgency=low "touchscreen Enabled"
+  notify-send --category=device --urgency=low "Touchscreen Enabled"
 }
 
 disable_touchscreen() {
   hyprctl keyword -r '$TOUCHSCREEN_ENABLED' "false"
   echo "false" >"$STATUS_FILE"
-  notify-send --category=device --urgency=low "touchscreen Disabled"
+  notify-send --category=device --urgency=low "Touchscreen Disabled"
 }
 
 if [ ! -f "$STATUS_FILE" ]; then
