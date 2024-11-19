fcdda4ce6e41d465a57e52683920b8f414f46c8a
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-11-19 23:03:24 +0530

    lazyvim: autosave autocmd for tex and latex files

--- .config/nvim-lazyvim/lua/config/autocmds.lua
+++ .config/nvim-lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,28 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Autosave
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
+
+-- Specify the filetypes for which autosave should be enabled
+local filetypes = { "tex", "latex" }
+
+-- Autosave for only specific file types
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+    save()
+  end,
+  pattern = "*." .. table.concat(filetypes, ",*."),
+  group = "AutoSave",
+})
