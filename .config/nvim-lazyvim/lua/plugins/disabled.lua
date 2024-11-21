fb64bf83ed1817598a0b537b8bda5e3dc203864d
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-11-21 21:53:54 +0530

    lazyvim: disable auto-install mason lsps for unsupported

--- .config/nvim-lazyvim/lua/plugins/disabled.lua
+++ .config/nvim-lazyvim/lua/plugins/disabled.lua
@@ -1,4 +1,32 @@
 return {
-  "folke/noice.nvim",
-  enabled = false,
+  {
+    "folke/noice.nvim",
+    enabled = false,
+
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {
+          mason = false
+        },
+        clangd = {
+          mason = false
+        },
+        texlab = {
+          mason = false
+        },
+        pyright = {
+          mason = false
+        },
+        bashls = {
+          mason = false
+        },
+        jsonls = {
+          mason = false
+        }
+      },
+    },
+  },
 }
