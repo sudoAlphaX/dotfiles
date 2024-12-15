e3b7b3f37755b944a411de17214b9dfebccf8eae
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-12-15 18:56:33 +0530

    lazyvim: disable auto-update check
    (cherry picked from commit bf39b977b4b6b2b103652917d5c5bbb8c7d53037)

--- .config/nvim-lazyvim/lua/config/lazy.lua
+++ .config/nvim-lazyvim/lua/config/lazy.lua
@@ -46,7 +46,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+  checker = { enabled = false, notify = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
