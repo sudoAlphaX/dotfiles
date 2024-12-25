75229b9c4384857474541a79817af7e4dcd238ba
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-12-25 16:56:53 +0530

    lazyvim: migrate telescope to fzf-lua
    (cherry picked from commit 657b0d4cb38de0eacaaf11964c0139b31b36aa0c)

--- .config/nvim-lazyvim/lua/plugins/telescope.lua
+++ .config/nvim-lazyvim/lua/plugins/telescope.lua
@@ -1,9 +1,11 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    { "<leader>/", vim.NIL },
-    { "<leader> ", vim.NIL },
-    { "<leader>bb", vim.NIL },
-    { "<leader>gb", vim.NIL },
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>/", vim.NIL },
+      { "<leader> ", vim.NIL },
+      { "<leader>bb", vim.NIL },
+      { "<leader>gb", vim.NIL },
+    },
   },
 }
