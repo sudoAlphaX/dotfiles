c9ba7de2a1910152b757941f2d684616370d8fda
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-01-14 15:44:50 +0530

    hypr: check systemd-ac-power command to set refresh rate on startup

--- .config/hypr/scripts/init-refresh-rate.sh
+++ .config/hypr/scripts/init-refresh-rate.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/sh
 
 lowrate=48
 maxrate=60
 
-if [ "$(cat /sys/class/power_supply/BAT0/status)" == 'Discharging' ]; then
-  hyprctl -r keyword '$refresh_rate' $lowrate
-else
+if systemd-ac-power; then
   hyprctl -r keyword '$refresh_rate' $maxrate
+else
+  hyprctl -r keyword '$refresh_rate' $lowrate
 fi
