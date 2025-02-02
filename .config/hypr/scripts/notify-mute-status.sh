7319ad7b8fcc04f6d266b44e5e1ad791a241e893
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-02-02 14:17:59 +0530

    hypr: send normal volume_event on unmute with overamplified icon

--- .config/hypr/scripts/notify-mute-status.sh
+++ .config/hypr/scripts/notify-mute-status.sh
@@ -5,5 +5,5 @@ volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d?\d?\d%' | tail --li
 if [ "$(pactl get-sink-mute @DEFAULT_SINK@ | cut -d ' ' -f 2)" = 'yes' ]; then
   notify-send --category=device --urgency=low --icon=audio-volume-muted-symbolic --expire-time 3000 --hint=int:value:"$volume" mute_event
 else
-  notify-send --category=device --urgency=low --icon=audio-volume-medium-symbolic --expire-time 3000 --hint=int:value:"$volume" unmute_event
+  notify-send --category=device --urgency=low --icon=audio-volume-overamplified-symbolic --expire-time 3000 --hint=int:value:"$volume" volume_event
 fi
