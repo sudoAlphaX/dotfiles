8d71d72c6339a5ccc840a72ac174b43f2350fd00
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-02-09 17:37:52 +0530

    lazyvim: add hardtime.nvim plugin

---
+++ .config/nvim-lazyvim/lua/plugins/habits.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "tris203/precognition.nvim",
+    lazy = false,
+    enabled = true,
+    keys = {
+      { "<leader>uP", "<cmd>lua require('precognition').toggle()<CR>", desc = "Toggle Precognition" },
+    },
+    opts = {
+      showBlankVirtLine = false,
+      disabled_fts = {
+        "man",
+        "gitcommit",
+      },
+    },
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    keys = {
+      { "<leader>uH", "<cmd>:Hardtime toggle<CR>", desc = "Toggle Precognition" },
+    },
+    opts = {
+      disable_mouse = false,
+      max_count = 5,
+    },
+  },
+}
