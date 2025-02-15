e9b4ce781194b8e2d95c1797412c706c85ba8d25
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-02-16 01:02:08 +0530

    hypr,dunst: use stack tag to improve volume notifications

--- .config/hypr/scripts/notify-mute-status.sh
+++ .config/hypr/scripts/notify-mute-status.sh
@@ -3,7 +3,7 @@
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d?\d?\d%' | tail --lines=1 | sed 's/%//g')
 
 if [ "$(pactl get-sink-mute @DEFAULT_SINK@ | cut -d ' ' -f 2)" = 'yes' ]; then
-  notify-send --category=device --urgency=low --icon=audio-volume-muted-symbolic --expire-time 3000 --hint=int:value:0 volume_event
+  notify-send --category=device --urgency=low --icon=audio-volume-muted-symbolic --expire-time 3000 --hint=int:value:"$volume" mute_event
 else
   notify-send --category=device --urgency=low --icon=audio-volume-overamplified-symbolic --expire-time 3000 --hint=int:value:"$volume" volume_event
 fi
