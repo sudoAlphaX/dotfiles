281987080c0afddc4d66992123edde6f9e51b047
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-02-16 01:16:06 +0530

    hypr: ignore all keybinds if safeeyes is in break

---
+++ .config/hypr/scripts/safeeyes-inhibit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function safeeyes-handler {
+
+  if [[ ${1:0:10} == "openwindow" ]]; then
+    if [[ $(cut -d "," -f 2,3,4 <<<"${1:12}") =~ ^special:safeeyes,safeeyes,SafeEyes-[0-9]$ ]]; then
+      hyprctl dispatch submap safeeyes
+
+    fi
+  elif [[ ${1} == "destroyworkspace>>special:safeeyes" ]]; then
+    hyprctl dispatch submap reset
+  fi
+}
+
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do safeeyes-handler "$line"; done
