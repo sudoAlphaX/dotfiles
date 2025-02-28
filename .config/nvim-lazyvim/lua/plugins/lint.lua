8c169b030dd1cad798ba8c85c87c7d19f4809710
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-02-28 15:52:38 +0530

    lazyvim: rename HOME to XDG_CONFIG_HOME
    rename the variable to address XDG_CONFIG_HOME properly added in 27d3ef7

--- .config/nvim-lazyvim/lua/plugins/lint.lua
+++ .config/nvim-lazyvim/lua/plugins/lint.lua
@@ -1,11 +1,11 @@
-local HOME = os.getenv("XDG_CONFIG_HOME")
+local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
 return {
   "mfussenegger/nvim-lint",
   optional = true,
   opts = {
     linters = {
       ["markdownlint-cli2"] = {
-        args = { "--config", HOME .. "/markdownlint/.markdownlint-cli2.yaml", "--" },
+        args = { "--config", XDG_CONFIG_HOME .. "/markdownlint/.markdownlint-cli2.yaml", "--" },
       },
     },
   },
