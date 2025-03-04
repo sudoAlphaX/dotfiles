bcb9582d88d0942d6ef55d5810515d6d13f45a01
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-03-04 21:10:32 +0530

    hypr: make unfocus-discord.sh general purpose

--- .config/hypr/scripts/unfocus-discord.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-# Script to unfocus Discord and focus the last focused application
-# To be executed on Hyprland startup
-
-# Wait until discord is the focused application
-until [ "$(hyprctl activewindow -j | jq .initialClass -r)" = "vesktop" ]; do sleep 0.1; done
-hyprctl dispatch focuscurrentorlast
