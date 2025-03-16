7c243a26f0cdad4b70d9df39ea7125f7e819c708
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-03-16 21:41:17 +0530

    hypr: use pgrep instead of pidof for delay start
    pidof doesnt work if current user is not root. Did not RTFM

--- .config/hypr/scripts/delay-start.sh
+++ .config/hypr/scripts/delay-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-until [ "$(pidof -x -c "$1")" ]; do sleep 1; done
+until [ "$(pgrep -x -u "$(whoami)" "$1")" ]; do sleep 1; done
 if [ $# -eq 3 ]; then sleep "$3"; else sleep 5; fi
 $2 &
