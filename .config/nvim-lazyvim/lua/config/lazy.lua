f7e834740e31a53ea6b9e074b7dbddfacb1dbba5
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-03-17 16:08:33 +0530

    Revert "lazyvim: use catppuccin-mocha as colorscheme in lazy"
    This reverts commit 320742142e959dee1d177bf2aefb8ad49ad322f0.
    Fix color scheme not reverting after installing a new plugin.
    
    (cherry picked from commit ab3f44af4fdf6e3a48e411b125961c07d8947029)

--- .config/nvim-lazyvim/lua/config/lazy.lua
+++ .config/nvim-lazyvim/lua/config/lazy.lua
@@ -48,7 +48,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin-mocha" } },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true, notify = false, frequency = 1209600 }, -- automatically check for plugin updates
   performance = {
     rtp = {
