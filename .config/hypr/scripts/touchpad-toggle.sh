9ea78352a65eeabc25514bf99bb0329739089340
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-04-08 17:04:13 +0530

    hypr: touchpad-toggle script do not show notification on explicit toggling

--- .config/hypr/scripts/touchpad-toggle.sh
+++ .config/hypr/scripts/touchpad-toggle.sh
@@ -4,15 +4,21 @@ STATUS_FILE=$XDG_RUNTIME_DIR/touchpad-status
 device="dll07d0:01-044e:120b"
 
 enable_touchpad() {
+  show_notification=${1:-true}
   hyprctl keyword "device[$device]:enabled" 1
-  echo "true" >$STATUS_FILE
-  notify-send --category=device --urgency=low "Touchpad Enabled"
+  echo "true" >"$STATUS_FILE"
+  if [ "$show_notification" = "true" ]; then
+    notify-send --category=device --urgency=low "Touchpad Enabled"
+  fi
 }
 
 disable_touchpad() {
+  show_notification=${1:-true}
   hyprctl keyword "device[$device]:enabled" 0
-  echo "false" >$STATUS_FILE
-  notify-send --category=device --urgency=low "Touchpad Disabled"
+  echo "false" >"$STATUS_FILE"
+  if [ "$show_notification" = "true" ]; then
+    notify-send --category=device --urgency=low "Touchpad Disabled"
+  fi
 }
 
 toggle_touchpad() {
@@ -27,9 +33,9 @@ toggle_touchpad() {
 if [ $# -eq 0 ]; then
   toggle_touchpad
 elif [ "$1" = "true" ]; then
-  enable_touchpad
+  enable_touchpad false
 elif [ "$1" = "false" ]; then
-  disable_touchpad
+  disable_touchpad false
 else
   echo "Error: Invalid argument. Use 'true', 'false', or no argument to toggle." >&2
   exit 1
