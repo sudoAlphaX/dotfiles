800d2438d81c6fb10f387742ea04a1dc27ea98cc
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-04-23 16:26:47 +0530

    lazyvim: refactor lazyvim autocmds

--- .config/nvim-lazyvim/lua/config/autocmds.lua
+++ .config/nvim-lazyvim/lua/config/autocmds.lua
@@ -2,45 +2,34 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Autosave
-local function save()
-  local buf = vim.api.nvim_get_current_buf()
+-- Autosave functionality
+-- Specify the filetypes for which autosave and keybindings should be enabled
+Autosave_filetypes = { "tex", "latex" }
 
-  vim.api.nvim_buf_call(buf, function()
-    vim.cmd("silent! write")
-  end)
-end
+vim.api.nvim_create_augroup("AutoSave", { clear = true })
 
-vim.api.nvim_create_augroup("AutoSave", {
-  clear = true,
-})
-
--- Specify the filetypes for which autosave should be enabled
-local filetypes = { "tex", "latex" }
-
--- Autosave for only specific file types
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  callback = function()
-    save()
-  end,
-  pattern = "*." .. table.concat(filetypes, ",*."),
   group = "AutoSave",
+  callback = function()
+    local ft = vim.bo.filetype
+    if vim.tbl_contains(Autosave_filetypes, ft) then
+      vim.cmd("silent! write")
+    end
+  end,
 })
 
--- Snippets
+-- Load custom snippets
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = "./snippets",
 })
 
--- Keybinding for specific filetypes
-vim.api.nvim_create_augroup("Keybinds", {
-  clear = true,
-})
+-- Special keybindings for LaTeX editing
+vim.api.nvim_create_augroup("Keybinds", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = filetypes,
+  group = "Keybinds",
+  pattern = { "tex", "latex", "markdown" },
   callback = function()
     vim.api.nvim_buf_set_keymap(0, "i", ";", "$", { noremap = true, silent = true })
   end,
-  group = "Keybinds",
 })
