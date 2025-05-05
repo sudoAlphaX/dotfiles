55e6b374b444d4b6b014fa2bf6316807d3b7676c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-05-06 01:05:35 +0530

    hypr: use local icon for autoconnect bluetooth

--- .config/hypr/scripts/bluetooth-autoconnect.sh
+++ .config/hypr/scripts/bluetooth-autoconnect.sh
@@ -3,8 +3,10 @@
 IFS='
 '
 
+icons_path="$HOME/.config/dunst/icons"
+
 devices=$(bluetoothctl devices Trusted)
 
 for device in $devices; do
-  bluetoothctl connect "$(echo "$device" | cut -d " " -f 2)" && notify-send -i bluetooth-symbolic -a bluetoothctl "Autoconnected to $(echo "$device" | cut -d " " -f 3)" && break
+  bluetoothctl connect "$(echo "$device" | cut -d " " -f 2)" && notify-send --icon="$icons_path"/bluetooth/bluetooth-symbolic.svg -symbolic -a bluetoothctl "Autoconnected to $(echo "$device" | cut -d " " -f 3)" && break
 done
