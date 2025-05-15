c24f0fc83f89e36d3948f4452a2e07ad8e2f52db
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-05-16 02:21:02 +0530

    hypr: bluetooth autoconnect script fix wrong arguments in notify-send command

--- .config/hypr/scripts/bluetooth-autoconnect.sh
+++ .config/hypr/scripts/bluetooth-autoconnect.sh
@@ -8,5 +8,5 @@ icons_path="$HOME/.config/dunst/icons"
 devices=$(bluetoothctl devices Trusted)
 
 for device in $devices; do
-  bluetoothctl connect "$(echo "$device" | cut -d " " -f 2)" && notify-send --icon="$icons_path"/bluetooth/bluetooth-symbolic.svg -symbolic -a bluetoothctl "Autoconnected to $(echo "$device" | cut -d " " -f 3)" && break
+  bluetoothctl connect "$(echo "$device" | awk '{print $2}')" && notify-send --icon="$icons_path"/bluetooth/bluetooth-symbolic.svg -a bluetoothctl "Autoconnected to $(echo "$device" | cut -d " " -f 3-)" && break
 done
