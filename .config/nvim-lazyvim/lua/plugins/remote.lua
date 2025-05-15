265ab7f2ecfc1889aac267737b77adfdd6e61d88
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-05-15 20:50:12 +0530

    lazyvim: add remote sshfs plugin

---
+++ .config/nvim-lazyvim/lua/plugins/remote.lua
@@ -0,0 +1,25 @@
+return {
+  "nosduco/remote-sshfs.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  lazy = true,
+  cmd = {
+    "RemoteSSHFSConnect",
+    "RemoteSSHFSDisconnect",
+    "RemoteSSHFSEdit",
+    "RemoteSSHFSFindFiles",
+    "RemoteSSHFSLiveGrep",
+  },
+  keys = {
+
+    { "<leader>fsc", "<ESC><CMD>lua require('remote-sshfs.api').connect()<CR>", desc = "Connect to remote" },
+    { "<leader>fsd", "<ESC><CMD>lua require('remote-sshfs.api').disconnect()<CR>", desc = "Disconnect from remote" },
+    {
+      "<leader>fse",
+      "<ESC><CMD>lua require('remote-sshfs.api').edit()<CR>",
+      desc = "Edit remote (SSH) configurations",
+    },
+    { "<leader>fsf", "<ESC><CMD>lua require('remote-sshfs.connections').find_files()<CR>", desc = "Remote Find Files" },
+    { "<leader>fsg", "<ESC><CMD>lua require('remote-sshfs.connections').live_grep()<CR>", desc = "Remote Live Grep" },
+  },
+  opts = {},
+}
