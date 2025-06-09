b96035d811a362c85e3974541b340797b86b3153
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-09 16:06:56 +0530

    lazyvim: automatic enable installed lsp servers
    (cherry picked from commit 50638dc1be0a7a988db381a4dc39662a73cd15c0)

--- dot-config/nvim-lazyvim/lua/plugins/lsp.lua
+++ dot-config/nvim-lazyvim/lua/plugins/lsp.lua
@@ -1,10 +1,18 @@
 return {
-  "saghen/blink.cmp",
-  optional = true,
-  opts = {
-    keymap = {
-      ["<C-j>"] = { "select_next", "fallback" },
-      ["<C-k>"] = { "select_prev", "fallback" },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      keymap = {
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+      },
+    },
+  },
+  {
+    "https://github.com/mason-org/mason-lspconfig.nvim",
+    opts = {
+      automatic_enable = true,
     },
   },
 }
