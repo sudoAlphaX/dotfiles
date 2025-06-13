b83ea7648e528f8bb48a46beabcea73c34b03546
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-13 23:17:13 +0530

    lazyvim: disable prettier formatter

--- dot-config/nvim-lazyvim/lua/config/lazy.lua
+++ dot-config/nvim-lazyvim/lua/config/lazy.lua
@@ -39,7 +39,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tex" },
 
     { import = "lazyvim.plugins.extras.formatting.black" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
 
     { import = "lazyvim.plugins.extras.dap.core" },
 
