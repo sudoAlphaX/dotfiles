ae9e1aac6f9217b7857e07d2c3ad1fda16b43755
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-14 00:09:52 +0530

    lazyvim: set plugin clone concurrency to 4 to avoid being killed by android

--- dot-config/nvim-lazyvim/lua/config/lazy.lua
+++ dot-config/nvim-lazyvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  concurrency = 4,
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
