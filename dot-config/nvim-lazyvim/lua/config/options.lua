58373597e926b4be28eb03b7f889e0727710bf00
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-17 18:32:46 +0530

    Revert "lazyvim: disabl animations"
    This reverts commit 64ccbd427a2bd2f57b3309ff336404040b54d68d.

--- dot-config/nvim-lazyvim/lua/config/options.lua
+++ dot-config/nvim-lazyvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.snacks_animate = false
+-- vim.g.snacks_animate = false
 -- vim.g.lazyvim_cmp = "nvim-cmp"
