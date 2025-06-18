73c7b6d6d8447a8b1f2ed24e7d20610c9f56cb1d
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-18 16:42:28 +0530

    lazyvim: use catppuccin colors for modes.nvim

--- dot-config/nvim-lazyvim/lua/plugins/colorscheme.lua
+++ dot-config/nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,12 @@ return {
     "mvllow/modes.nvim",
     commit = "main",
     opts = {
+      colors = {
+        copy = "#f9e2af",
+        delete = "#f38ba8",
+        insert = "#a6e3a1",
+        visual = "#cba6f7",
+      },
       set_number = false,
     },
   },
