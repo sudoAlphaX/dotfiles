54b26b6ead8b4885281153dc2d85d45851fa5083
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-18 16:52:50 +0530

    lazyvim: add all buffers to copilot context
    (cherry picked from commit f4ceac8fdce5d22991af463a112b1cfd155600f7)

--- dot-config/nvim-lazyvim/lua/plugins/copilot.lua
+++ dot-config/nvim-lazyvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   "CopilotC-Nvim/CopilotChat.nvim",
   optional = true,
   opts = {
-    context = "buffer",
+    context = "buffers",
     model = "claude-3.7-sonnet",
     insert_at_end = true,
     auto_insert_mode = true,
