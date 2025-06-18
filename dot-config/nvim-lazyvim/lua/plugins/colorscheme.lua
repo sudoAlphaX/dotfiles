770b6da1292329f421dbd4804b1e5c039c3395dd
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-18 20:01:32 +0530

    lazyvim: modes.nvim add custom color for change mode

--- dot-config/nvim-lazyvim/lua/plugins/colorscheme.lua
+++ dot-config/nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -11,6 +11,7 @@ return {
     opts = {
       colors = {
         copy = "#f9e2af",
+        change = "#94e2d5",
         delete = "#f38ba8",
         insert = "#a6e3a1",
         visual = "#cba6f7",
