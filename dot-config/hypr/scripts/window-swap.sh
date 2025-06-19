4787d552576b4e77fc0199765d19c33d2c48edce
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-06-19 23:12:35 +0530

    hypr: rework on window movement keybinds

--- dot-config/hypr/scripts/window-swap.sh
+++ dot-config/hypr/scripts/window-swap.sh
@@ -7,8 +7,7 @@ activewindow=$(hyprctl -j activewindow)
 isFloat=$(echo "$activewindow" | jq .floating)
 
 if [ "$isFloat" = true ]; then
-  declare -A movemaps=(["l"]=" -$value 0" ["r"]="$value 0" ["u"]="0 -$value" ["d"]="0 $value")
-  hyprctl dispatch moveactive "${movemaps[$direction]}"
-else
   hyprctl dispatch movewindow "$direction"
+else
+  hyprctl dispatch swapwindow "$direction"
 fi
