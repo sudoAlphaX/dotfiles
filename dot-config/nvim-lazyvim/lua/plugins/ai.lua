cc7ec241187ffdf46db4065f58292536f49b96a2
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-10-26 23:21:19 +0530

    lazyvim: copilot use sonnet 4 model as default

---
+++ dot-config/nvim-lazyvim/lua/plugins/ai.lua
@@ -0,0 +1,12 @@
+return {
+  {
+
+    "CopilotC-Nvim/CopilotChat.nvim",
+    optional = true,
+    opts = {
+      context = "buffers",
+      model = "claude-sonnet-4",
+      auto_insert_mode = true,
+    },
+  },
+}
