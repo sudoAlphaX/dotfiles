ca9169b8785991492e97a7d69b52b8c1cecda984
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-11-01 01:08:45 +0530

    lazyvim: add sidekick.nvim and use copilot-chat in inline

--- dot-config/nvim-lazyvim/lua/plugins/ai.lua
+++ dot-config/nvim-lazyvim/lua/plugins/ai.lua
@@ -1,12 +1,29 @@
 return {
   {
-
     "CopilotC-Nvim/CopilotChat.nvim",
     optional = true,
+    keys = {
+      {
+        "<leader>al",
+        function()
+          require("CopilotChat").open({
+            selection = require("CopilotChat.select").buffer,
+            window = {
+              layout = "float",
+              relative = "cursor",
+              width = 1,
+              height = 0.4,
+              row = 1,
+            },
+          })
+        end,
+      },
+    },
     opts = {
-      context = "buffers",
+      selection = require("CopilotChat.select").buffer,
       model = "claude-sonnet-4",
       auto_insert_mode = true,
+      auto_fold = true,
     },
   },
 }
