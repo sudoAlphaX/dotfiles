79d1ed888832e191ce44e8fadb04ea4de18287e7
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-11-09 02:54:16 +0530

    lazyvim: precognition.nvim show only when idle for 1 s

--- dot-config/nvim-lazyvim/lua/plugins/habits.lua
+++ dot-config/nvim-lazyvim/lua/plugins/habits.lua
@@ -1,24 +1,65 @@
 return {
+
   {
     "tris203/precognition.nvim",
     lazy = false,
-    enabled = false,
-    keys = {
-      {
-        "<leader>uP",
-        function()
-          require("precognition").toggle()
-        end,
-        desc = "Toggle Precognition",
-      },
-    },
+    enabled = true,
     opts = {
       showBlankVirtLine = false,
-      disabled_fts = {
-        "man",
-        "gitcommit",
-      },
+      disabled_fts = { "man", "gitcommit" },
     },
+    config = function(_, opts)
+      local precog = require("precognition")
+      precog.setup(opts)
+      precog.hide() -- start hidden
+
+      local idle_ms = 1000
+      local timer = vim.uv.new_timer()
+      local shown = false
+      local enabled = true -- default ON
+
+      vim.on_key(function()
+        if not enabled then
+          return
+        end -- respect toggle
+
+        if timer:is_active() then
+          timer:stop()
+        end
+        if shown then
+          shown = false
+          pcall(precog.hide)
+        end
+
+        timer:start(idle_ms, 0, function()
+          vim.schedule(function()
+            if enabled then
+              shown = true
+              pcall(precog.show)
+            end
+          end)
+        end)
+      end, vim.api.nvim_create_namespace("precognition-idle"))
+
+      -- Snacks toggle
+      vim.schedule(function()
+        Snacks.toggle({
+          name = "Precognition",
+          get = function()
+            return enabled
+          end,
+          set = function(state)
+            enabled = state
+            if not state then
+              if timer:is_active() then
+                timer:stop()
+              end
+              pcall(precog.hide)
+            end
+          end,
+        }):map("<leader>uP")
+      end)
+    end,
   },
   {
     "m4xshen/hardtime.nvim",
