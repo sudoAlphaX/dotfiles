45ba8946a37afd4781f55436c91e7874224496cc
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-12-23 23:49:52 +0530

    lazyvim: add better inline diag plugin

--- dot-config/nvim-lazyvim/lua/plugins/lsp.lua
+++ dot-config/nvim-lazyvim/lua/plugins/lsp.lua
@@ -48,4 +48,44 @@ return {
       mode = "topline",
     },
   },
+  {
+    {
+      "rachartier/tiny-inline-diagnostic.nvim",
+      event = "VeryLazy",
+      priority = 1000,
+      opts = {
+        preset = "classic",
+
+        transparent_bg = true,
+
+        disabled_ft = {},
+
+        options = {
+
+          use_icons_from_diagnostic = false,
+          set_arrow_to_diag_color = true,
+
+          multilines = {
+            enabled = true,
+            always_show = false,
+            trim_whitespaces = true,
+            severity = { vim.diagnostic.severity.ERROR },
+          },
+
+          show_all_diags_on_cursorline = false,
+
+          -- Experimental options, subject to misbehave in future NeoVim releases
+          experimental = {
+            -- Make diagnostics not mirror across windows containing the same buffer
+            -- See: https://github.com/rachartier/tiny-inline-diagnostic.nvim/issues/127
+            use_window_local_extmarks = false,
+          },
+        },
+      },
+    },
+    {
+      "neovim/nvim-lspconfig",
+      opts = { diagnostics = { virtual_text = false } },
+    },
+  },
 }
