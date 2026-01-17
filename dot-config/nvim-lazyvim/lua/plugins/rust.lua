216c39d80dbe342434379abbe3dc7ce6e20f57f4
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2026-01-17 22:39:47 +0530

    lazyvim: rustowl load plugin only on keybind

--- dot-config/nvim-lazyvim/lua/plugins/rust.lua
+++ dot-config/nvim-lazyvim/lua/plugins/rust.lua
@@ -1,12 +1,34 @@
 return {
   "cordx56/rustowl",
   version = "*", -- Latest stable version
-  build = "cargo binstall rustowl",
   ft = { "rust" },
+  lazy = true,
 
   opts = {
-    auto_enable = true,
+    auto_attach = false,
+    auto_enable = false,
     idle_time = 500,
     highlight_style = "underline",
   },
+
+  config = function(_, opts)
+    local rustowl = require("rustowl")
+    rustowl.setup(opts)
+
+    vim.schedule(function()
+      Snacks.toggle({
+        name = "RustOwl",
+        get = function()
+          return rustowl.is_enabled()
+        end,
+        set = function(state)
+          if state then
+            rustowl.enable()
+          else
+            rustowl.disable()
+          end
+        end,
+      }):map("<leader>co")
+    end)
+  end,
 }
