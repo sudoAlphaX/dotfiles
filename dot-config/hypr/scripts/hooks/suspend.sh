1b22414dd25c0127d8dcb748d966f3c18306b929
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2026-07-16 08:34:57 +0530

    hypr: rename *-suspend.d to suspend-*.d

--- dot-config/hypr/scripts/hooks/suspend.sh
+++ dot-config/hypr/scripts/hooks/suspend.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/sh
 
-PRE_DIR="$(dirname "$0")/pre-suspend.d"
-POST_DIR="$(dirname "$0")/post-suspend.d"
+PRE_DIR="$(dirname "$0")/suspend-pre.d"
+POST_DIR="$(dirname "$0")/suspend-post.d"
 
 if [ "$1" = "pre" ]; then
   if [ -d "$PRE_DIR" ]; then
     for script in "$PRE_DIR"/*; do
       if [ -x "$script" ]; then
         echo "Running pre suspend hook: $(basename "$script")"
-        "$script" &
+        "$script"
       fi
     done
   fi
@@ -18,7 +18,7 @@ elif [ "$1" = "post" ]; then
     for script in "$POST_DIR"/*; do
       if [ -x "$script" ]; then
         echo "Running post suspend hook: $(basename "$script")"
-        "$script" &
+        "$script"
       fi
     done
   fi
