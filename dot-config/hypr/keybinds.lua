01a7ddc781eed55d1a0ff850f10883563e268f53
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2026-08-11 08:40:17 +0530

    hypr: xf86audioplay play-pause instead of only play

--- dot-config/hypr/keybinds.lua
+++ dot-config/hypr/keybinds.lua
@@ -260,7 +260,7 @@ hl.bind(
 )
 
 -- media controls
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play"), { locked = true, submap_universal = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true, submap_universal = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl pause"), { locked = true, submap_universal = true })
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true, submap_universal = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true, submap_universal = true })
